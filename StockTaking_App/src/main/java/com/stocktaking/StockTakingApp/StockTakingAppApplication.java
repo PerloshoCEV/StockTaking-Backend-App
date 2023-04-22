f3292e6c70fdf9d0c854f5297cca1d08fca5d33a
Diogenes Alcina <127620996+PerloshoCEV@users.noreply.github.com>
2023-04-22 15:57:00 +0200

    Init Maven Project

---
+++ StockTaking_App/src/main/java/com/stocktaking/StockTakingApp/StockTakingAppApplication.java
@@ -0,0 +1,13 @@
+package com.stocktaking.StockTakingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTakingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTakingAppApplication.class, args);
+	}
+
+}
---
+++ StockTaking_App/src/test/java/com/stocktaking/StockTakingApp/StockTakingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocktaking.StockTakingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTakingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
